775c8ee581e4e97aa8fb123373be68432de4bfe0
Yuta Imai <0625yutayuta@gmail.com>
2018-09-18 15:42:58 +0900

    first commit

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,60 @@
+<?php namespace App\Http\Controllers;
+
+	use App\Article;
+	use App\Http\Requests;
+	use App\Http\Controllers\Controller;
+
+	use Illuminate\Http\Request;
+
+	class ArticlesController extends Controller
+	{
+
+	   /**
+	    * @var Article
+	    */
+	   protected $article;
+
+	   /**
+	    * @param Article $article
+	    */
+	   public function __construct(Article $article)
+	   {
+	       $this->article = $article;
+	   }
+
+	   public function getIndex()
+	   {
+
+	   }
+
+	   public function getShow()
+	   {
+
+	   }
+
+	   public function getCreate()
+	   {
+
+	   }
+
+	   public function postCreate()
+	   {
+
+	   }
+
+	   public function getEdit()
+	   {
+
+	   }
+
+	   public function postEdit()
+	   {
+
+	   }
+
+	   public function postDelete()
+	   {
+
+	   }
+
+	}
---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,27 @@
+@extends('app')
+
+	@section('content')
+	    <h2 class="page-header">記事一覧</h2>
+	    <table class="table table-striped table-hover">
+	        <thead>
+	        <tr>
+	            <th>タイトル</th>
+	            <th>本文</th>
+	            <th>作成日時</th>
+	            <th>更新日時</th>
+	            <th></th>
+	        </tr>
+	        </thead>
+	        <tbody>
+	        @foreach($articles as $article)
+	            <tr>
+                   <td>{{{ $article->title }}}</td>
+                   <td>{{{ $article->body }}}</td>
+                   <td>{{{ $article->created_at }}}</td>
+                   <td>{{{ $article->updated_at }}}</td>
+                   <td><a href="/articles/show/{{{ $article->id }}}" class="btn btn-default btn-xs">詳細</a></td>
+	            </tr>
+	        @endforeach
+	        </tbody>
+	    </table>
+	@endsection
